f1f4149f8127750463cba6bb7324b629185b4a74
joshuaday98 <joshua.f.day@gmail.com>
2018-11-19 09:08:10 -0800

    creating class in lottov2 to better manage global variables

--- lotteryv2.py
+++ lotteryv2.py
@@ -1,6 +1,9 @@
 from secrets import randbelow
 
 
+def stringify_ticket_num(num):
+    return "-".join(map(str, num))
+
 def gen_ticket_num():
     nums = [[]]
     total = 0
@@ -18,15 +21,16 @@ class Lotto():
     def __init__(self, match_threshold, powerball):
         self.match_threshold = match_threshold
         self.matches = 0
+
         self.cost = powerball
-        self.pb_bool = True if powerball == 3 else False
+        self.pb_bool = True if powerball == 2 else False
 
         self.drawn_num = gen_ticket_num()
         self.money_spent = 0
 
 
     def compare_nums(self, ticket_num):
-        #comppare ticket numbers
+        #compare ticket numbers
         self.matches = 0
         results = []
 
@@ -41,10 +45,59 @@ class Lotto():
         #check powerball results
         if self.pb_bool:
             results.append(True if self.drawn_num[1] == ticket_num[1] else False)
-
+        else:
+            results.append(True)
 
         return results
 
+    def winnings(self, ticket_num, pb_result):
+        rewards_dict = {
+            "True": {
+                1: 4,
+                2: 7,
+                3: 100,
+                4: 50000,
+                5: 124000000
+            },
+            "False": {
+                1: 4,
+                2: 4,
+                3: 7,
+                4: 100,
+                5: 1000000
+            }
+        }
+
+        pb_result_text = ".\n"
+
+        str_drawn_num = stringify_ticket_num(ticket_num[0])
+        str_ticket_num = stringify_ticket_num(ticket_num[0])
+        print(pb_result)
+        earnings = rewards_dict[str(pb_result)][self.matches]
+        profit = (self.money_spent * -1) + earnings
+
+        if self.pb_bool:
+            pb_reuslt_text = f"""
+                You won the Powerball!
+                Drawing Powerball: {self.drawn_num[1]}
+                Your Powerball: {ticket_num[1]}
+            """
+
+        return f"""
+            You hit a match!
+            The lottery Number was : {str_drawn_num}
+            Your Winning Number was: {str_ticket_num} with {self.matches}!
+            You bought {self.money_spent/self.cost} tickets.\n
+            .\n
+            {pb_result_text}
+            .\n
+            Money Stats:
+            Spent   : ${self.money_spent}
+            Winnings: ${earnings}
+            Profit  : ${profit}
+
+            You could buy roughly {profit/1.86} eggs with that money!
+        """
 
 
 def main():
@@ -61,18 +114,19 @@ def main():
     print(f"Your number is {lotto_roll.drawn_num}")
 
     lotto_result = [False, False]
+    try:
+        while False in lotto_result:
+            ticket_num = gen_ticket_num()
+            lotto_result = lotto_roll.compare_nums(ticket_num)
 
-    while False in lotto_result:
-        ticket_num = gen_ticket_num()
-        lotto_result = lotto_roll.compare_nums(ticket_num)
+            print(f"Your ticket number is {ticket_num} with {lotto_roll.matches} match!")
 
-        print(f"Your ticket number is {ticket_num} with {lotto_roll.matches} match!")
+        #lotto_result[1] == powerball result
+        print(lotto_roll.winnings(ticket_num, lotto_result[1]))
 
-    print(f"""
-        Drawn Lotto Number:  {lotto_roll.drawn_num}
-        Winning Number:      {ticket_num}
-        # of tickets bought: {lotto_roll.money_spent / lotto_roll.cost}
-        Total $$$ spent:     {lotto_roll.money_spent}
+    except KeyboardInterrupt:
+        print(f"""\nThe starting number was:{lotto_roll.drawn_num}\n
+        You bought {lotto_roll.money_spent/lotto_roll.cost} lotto tickets and spent ${lotto_roll.money_spent}.
         """)
 
 
